5c57dedf1ec1f4ee36ea3c467051345a0f72cbb7
y0han35u <y0han35u@gmail.com>
2021-01-27 01:52:28 +0900

    modified vue file

--- app/cipher-app/src/components/CaesarCipher.vue
+++ app/cipher-app/src/components/CaesarCipher.vue
@@ -1,4 +1,8 @@
 <template>
+    <div class="header">
+        <button @click="doLogin">{{ data.status_msg }}</button>
+        <p v-if="data.user">Hello! {{ data.user }}</p>
+    </div>
     <h1>Caesar Cipher</h1>
     <textarea v-model="data.text" name="contents" id="contents" cols="30" rows="10" placeholder="Enter your text."></textarea>
     <div class="selector">
@@ -24,15 +28,38 @@
 
     <button @click="doAction(data.text, data.rot_key, data.ed_flag)">GO</button>
 
-    <div>
+    <div class="list" v-if="data.user">
         <ul>
-          <li v-for="(item, index) in data.text_list" :key="index">{{ item }}</li> 
+          <li v-for="(item, key) in data.fire_data" :key="key">{{ item.text }}</li> 
+        </ul>
+    </div>
+id
+    <div class="list2" v-if="data.user == null">
+        <ul>
+            <li v-for="item in data.obj_list" :key="item.key">{{ item.text }}</li>
         </ul>
     </div>
 </template>
 
 <script>
 import { reactive } from 'vue'
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+   apiKey: "AIzaSyCUhuV-5NE699PnLQ4akulxUsa-giMM0VY",
+   authDomain: "cipher-app-71877.firebaseapp.com",
+   projectId: "cipher-app-71877",
+   storageBucket: "cipher-app-71877.appspot.com",
+   messagingSenderId: "628117390597",
+   appId: "1:628117390597:web:d9ec107576e3b5bdbc636c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var provider = new firebase.auth.GoogleAuthProvider()
+//const fdata = firebase.database().ref('data/')
+
 
 export default {
     setup() {
@@ -40,11 +67,13 @@ export default {
             text: '',
             rot_key: 0,
             ed_flag: '',
-            text_list: [],
+            obj_list: [],
+            status_msg: 'Login',
+            user: null,
+            fire_data: {},
         })
 
         const CaesarCipher = (str, rot, x)=> {
-            console.log(str, rot, x)
             var lowerStr = str.toLowerCase();
             var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
             var result = "";
@@ -79,12 +108,90 @@ export default {
             return result;
         }
 
-        const doAction = (text, rot_key, ed_flag)=> {
-            console.log(text, rot_key, ed_flag)
-            data.text_list.unshift(CaesarCipher(text, rot_key, ed_flag));
+        const getCreateTimeId = ()=> {
+            let d = new Date()
+            let dstr = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
+            let id = d.getTime()
+            return {time: dstr, id: id}
         }
 
-        return { data, CaesarCipher, doAction }
+        const doAction = (text, rot_key, ed_flag)=> {
+            console.log(text, rot_key, ed_flag)
+            let user = firebase.auth().currentUser
+            let timeId = getCreateTimeId()
+            let id = timeId.id
+            let obj = {
+                text: CaesarCipher(text, rot_key, ed_flag),
+                user: user.displayName,
+                posted: timeId.time,
+                cryptType: 'CaesarCipher',
+            }
+            if(data.user != null){
+                firebase.database().ref('data/' + id).set(obj)
+                data.obj_list = []
+            } else {
+                let lcl_obj = {
+                    text: CaesarCipher(text, rot_key, ed_flag),
+                    id: id,
+                    posted: timeId.time,
+                    cryptType: 'CaesarCipher',
+                }
+                data.obj_list.unshift(lcl_obj)
+            }
+            
+        }
+
+        const login = ()=> {
+            firebase.auth().signInWithPopup(provider).then((result)=> {
+                data.user = result.user
+                if (data.obj_list.length) {
+                    for(let item in data.obj_list){
+                        let id = item.id
+                        let obj = {
+                            text: item.text,
+                            user: firebase.auth().currentUser.displayName,
+                            posted: item.time,
+                            cryptType: 'CaesarCipher',
+                        }
+                        firebase.database().ref('data/' + id).set(obj)
+                    }
+                    data.obj_list = []
+                }
+                firebase.database().ref('data').once('value', (snapshot)=>{
+                    if (firebase.auth().currentUser != null) {
+                        console.log(snapshot)
+                        let arr = []
+                        let result = snapshot.val()
+                        console.log(result)
+                        for(let item in result){
+                            arr.unshift(result[item])
+                        }
+                        console.log(arr)
+                        data.fire_data = arr
+                        data.status_msg = 'Logout'
+                    } else {
+                        data.fire_data = {}
+                    }
+                })
+            })
+        }
+
+        const logout = ()=> {
+            firebase.auth().signOut()
+            data.user = null
+            data.status_msg = 'Login'
+            data.obj_list = []
+        }
+
+        const doLogin = ()=> {
+            if(firebase.auth().currentUser == null){
+                login()
+            } else {
+                logout()
+            }
+        }
+
+        return { data, CaesarCipher, doAction, login, logout, doLogin, getCreateTimeId }
     }
 }
 </script>
